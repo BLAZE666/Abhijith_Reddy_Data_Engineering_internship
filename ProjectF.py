0ab967043f84b83d2493d0abf4a21f9e41ecce09
sahil <sahilsaha1420@gmail.com>
2024-04-28 23:01:56 +0530

    The final file

---
+++ ProjectF.py
@@ -0,0 +1,66 @@
+print('Enter district: Options- Adilabad, Jagtial')
+x = input().upper()
+if x == 'ADILABAD':
+    print('Enter Mandal: Options- Bela, Ichoda')
+    y = input().upper()
+    if y == 'BELA':
+        print('Enter Village: Options- Awalpur, Bhadi')
+        z = input().upper()
+        if z == 'AWALPUR':
+            print('The survey number is: 3, 45, 67')
+elif x == 'ADILABAD':
+    print('Enter Mandal: Options- Bela, Ichoda')
+    y = input().upper()
+    if y == 'BELA':
+        print('Enter Village: Options- Awalpur, Bhadi')
+        z = input().upper()
+        if z == 'BHADI':
+            print('The survey number is: 6, 78, 456')
+elif x == 'ADILABAD':
+    print('Enter Mandal: Options- Bela, Ichoda')
+    y = input().upper()
+    if y == 'ICHODA':
+        print('Enter Village: Options: Chincholi, Gundi')
+        z = input().upper()
+        if z == 'CHINCHOLI':
+            print('The survey number is: 7, 73, 461')
+elif x == 'ADILABAD':
+    print('Enter Mandal: Options- Bela, Ichoda')
+    y = input().upper()
+    if y == 'ICHODA':
+        print('Enter Village: Chincholi, Gundi')
+        z = input().upper()
+        if z == 'GUNDI':
+            print('The survey number is: 56, 988, 468')
+elif x == 'JAGTIAL':
+    print('Enter Mandal: Options- Beerpur, Mallapur')
+    y = input().upper()
+    if y == 'BEERPUR':
+        print('Enter Village: Options- Kolvai, Mangela')
+        z = input().upper()
+        if z == 'KOLVAI':
+            print('The survey number is: 95, 934, 784')
+elif x == 'JAGTIAL':
+    print('Enter Mandal: Options- Beerpur, Mallapur')
+    y = input().upper()
+    if y == 'BEERPUR':
+        print('Enter Village: Options- Kolvai, Mangela')
+        z = input().upper()
+        if z == 'MANGELA':
+            print('The survey number is: 902, 67, 90')
+elif x == 'JAGTIAL':
+    print('Enter Mandal: Options- Beerpur, Mallapur')
+    y = input().upper()
+    if y == 'MALLAPUR':
+        print('Enter Village: Options- Laxmipur, Sirpur')
+        z = input().upper()
+        if z == 'LAXMIPUR':
+            print('The survey number is: 898, 56, 33')
+elif x == 'JAGTIAL':
+    print('Enter Mandal: Options- Beerpur, Mallapur')
+    y = input().upper()
+    if y == 'MALLAPUR':
+        print('Enter Village: Options- Laxmipur, Sirpur')
+        z = input().upper()
+        if z == 'SIRPUR':
+            print('The survey number is: 904, 820, 88')
